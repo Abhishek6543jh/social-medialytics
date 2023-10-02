0b06bf98948b24c37e543a954c4aba6ea84ff8cb
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-10-02 16:12:59 +0530

    added login and reg

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const axios = require('axios'); // Import axios for making HTTP requests
 const ejs = require('ejs');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,14 +14,76 @@ app.set('view engine', 'ejs');
 
 // Serve static files from a 'public' directory (create this directory and put your CSS and JS files there)
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({extended:true}));
+//database connection
+mongoose.connect("mongodb+srv://abhishek24:Abhishek20@abh.sr77f7j.mongodb.net/details?retryWrites=true&w=majority").then(()=>{
+  console.log("connected")}
+)
 // Define a route for the root path that renders the form
 
+
+// database schema
+const userSchema = new mongoose.Schema({
+  username:String,
+  email:String,
+  password:String
+})
+
+
+///function to upload data to db when regesters
+
+const user = mongoose.model("userdetail",userSchema)
+const uploadtodb = async (req,res)=>{
+
+await user.create({ 
+username:req.body["username"],
+email:req.body["email"],
+password:req.body["password"] })
+}
+//function to chreck and authenticate login data
+const loginauth =async (res,username,password)=>{
+  const result = await user.exists({username:username,password:password})
+  if(result){
+    res.redirect("/home")
+  }
+  else{
+    res.redirect("/login")
+  }
+}
+
+
+
+
 app.get('/', (req, res) => {
+  res.render('login.ejs');
+});
+app.get('/home', (req, res) => {
   res.render('index.ejs');
 });
 
-// Handle the POST request for data submission
+
+app.get("/regester",(req,res)=>{
+  res.render("reg.ejs");
+});
+app.get("/login",(req,res)=>{
+  res.render("login.ejs");
+})
+
+
+// Handle the POST request for data submission for login
+app.post("/login/auth",(req,res)=>{
+  const user = req.body["username"];
+  const pass= req.body["password"];
+  loginauth(res,user,pass)
+ 
+});
+
+// Handle the POST request for data submission for registration
+app.post("/regester/reg",(req,res)=>{
+  uploadtodb(req,res)
+  res.redirect("/login");
+});
+
 // Handle the POST request for data submission
 app.post('/dataobtain', async (req, res) => {
     const instagramUsername = req.body.instagramUsername;
